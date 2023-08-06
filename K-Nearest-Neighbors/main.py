b28d0b00618ece20ef7f2359c0f8d81e0364d477
Tuan Vuong Minh <vuongminhtuan10@gmail.com>
2023-08-06 15:38:30 +0700

    KNN-Final

--- K-Nearest-Neighbors/main.py
+++ K-Nearest-Neighbors/main.py
@@ -1,15 +1,25 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from model import KNN_Classifier
+from model import KNN_Classifier, KNN_Regression
 
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
+# K-Nearest Neighbors Classification
 model = KNN_Classifier()
 model.fit(X_train, y_train)
 y_predicted = model.predict(X_test)
 
-print(y_predicted)
-print(model.score(X_test, y_test))
+print("The predicted labels: ", y_predicted)
+print("The accuracy of our model: {}%".format(model.score(X_test, y_test) *100))
+
+
+# # K-Nearest Neighbors Regression
+# model = KNN_Regression()
+# model.fit(X_train, y_train)
+# y_predicted = model.predict(X_test)
+
+# print("The predicted labels: ", y_predicted)
+# print("The accuracy of our model: {}%".format(model.score(X_test, y_test) *100))
--- K-Nearest-Neighbors/model.py
+++ K-Nearest-Neighbors/model.py
@@ -4,6 +4,7 @@ class KNN_Classifier:
         self.k = n_neighbors
         self.p = p
 
+    # Calculate the distance between 2 data points
     def __distance(self, data, X):
         dim = len(X)
         s = 0
@@ -13,6 +14,7 @@ class KNN_Classifier:
 
         return pow(s, 1/self.p)
     
+    # Find the k-neighbors which is nearest to given point
     def __kneighbors(self, X, n_neighbors):
         num_samples = len(self.X_train)
         samples_dist = []
@@ -25,15 +27,16 @@ class KNN_Classifier:
 
         return samples_dist[:n_neighbors]
 
+    # Fit train dataset to model
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
 
+    # Make a prediction of test dataset
     def predict(self, X):
-        labels = []
-        num_samples = len(X)
+        prediction_list = []
 
-        for i in range(num_samples):
+        for i in range(len(X)):
             k_neighbors = self.__kneighbors(X[i], self.k)
             checking = {}
 
@@ -46,11 +49,12 @@ class KNN_Classifier:
                     checking[self.y_train[index]] = 1
 
             checking = dict(sorted(checking.items(), key=lambda item: item[1]))
-            labels.append(list(checking.keys()).pop())
+            prediction_list.append(list(checking.keys()).pop())
 
-        return labels
+        return prediction_list
 
 
+    # Measure the accuracy of model
     def score(self, X, y):
         y_predicted = self.predict(X)
         count = 0
@@ -65,4 +69,70 @@ class KNN_Classifier:
 
 # Regression
 class KNN_Regression:
-    pass
+    def __init__(self, n_neighbors= 5, p= 2):
+        self.k = n_neighbors
+        self.p = p
+
+    # Calculate the distance between 2 data points
+    def __distance(self, data, X):
+        dim = len(X)
+        s = 0
+
+        for i in range(dim):
+            s = s + pow(data[i] - X[i], self.p)
+
+        return pow(s, 1/self.p)
+
+    # Find the k-neighbors which is nearest to given point
+    def __kneighbors(self, X, n_neighbors):
+        num_samples = len(self.X_train)
+        samples_dist = []
+
+        for i in range(num_samples):
+            dist = self.__distance(X, self.X_train[i])
+            samples_dist.append((dist, i))
+
+        samples_dist = sorted(samples_dist, key= lambda x : x[0])
+
+        return samples_dist[:n_neighbors]
+
+    # Fit train dataset to model
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    # Make a prediction of test dataset
+    def predict(self, X):
+        prediction_list =  []
+
+        for i in range(len(X)):           
+            k_neighbors = self.__kneighbors(X[i], self.k)
+            l = 0
+
+            for neighbor in k_neighbors:
+                index = neighbor[1]
+                l += self.y_train[index]
+
+            prediction = l / self.k
+
+            prediction_list.append(prediction)
+
+        return prediction_list
+
+    # Measure the accuracy of model
+    def score(self, X, y):
+        y_predicted = self.predict(X)
+        y_mean = 0
+        RSS = 0
+        TSS = 0
+
+        for l in y:
+            y_mean += l
+
+        y_mean = y_mean/len(y)
+
+        for i in range(len(y)):
+            RSS += pow(y[i] - y_predicted[i], 2)
+            TSS += pow(y[i] - y_mean, 2)
+
+        return (1 - (RSS/TSS))
