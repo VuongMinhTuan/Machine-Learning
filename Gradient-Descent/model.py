8d90f26a4bb04c2e9a7fa6408a3ac7a88b835973
Tuan Vuong Minh <vuongminhtuan10@gmail.com>
2023-08-21 14:33:00 +0700

    GD

---
+++ Gradient-Descent/model.py
@@ -0,0 +1,19 @@
+
+
+
+
+class LR_GD:
+    def __init__(self):
+        pass
+
+
+    def fit(self, X, y):
+        pass
+
+
+    def predict(self, X):
+        pass
+
+
+    def score(self, X, y):
+        pass
