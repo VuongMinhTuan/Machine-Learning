51717867111f41eea5cc2bc45447a12a0073915c
Tuan Vuong Minh <vuongminhtuan10@gmail.com>
2023-08-21 15:51:41 +0700

    Final

--- Gradient-Descent/main.py
+++ Gradient-Descent/main.py
@@ -1,6 +1,20 @@
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
+from sklearn import linear_model
+from model import LinearRegression
 
 X, y = datasets.make_regression(n_samples=100, n_features=2, noise=20, random_state=4)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
+# Linear Regression using Gradient Descent
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+print("The accuracy of my model: {}%".format(model.score(X_test, y_test) *100))
+
+
+# Using Sklearn libraries
+model = linear_model.LinearRegression()
+model.fit(X_train, y_train)
+
+print("The accuracy of Sklearn's model: {}%".format(model.score(X_test, y_test) *100))
--- Gradient-Descent/model.py
+++ Gradient-Descent/model.py
@@ -1,19 +1,55 @@
+import numpy as np
 
 
 
-
-class LR_GD:
-    def __init__(self):
+class LinearRegression:
+    def __init__(self, lr= 0.1, max_iters= 1000):
+        self.__w = None
+        self.__b = None
+        self.__lr = lr
+        self.__n_iters = max_iters
         pass
 
 
     def fit(self, X, y):
-        pass
+        n_samples, n_features = X.shape
+        self.__w = np.zeros(n_features)
+        self.__b = 0
+        it = 0
+
+        for _ in range(self.__n_iters):
+            it += 1
+
+            dw = (1/n_samples) * np.dot(X.T, np.dot(X, self.__w) + self.__b - y)
+            db = (1/n_samples) * np.sum(np.dot(X, self.__w) + self.__b - y)
+
+            w_new = self.__w - np.dot(self.__lr, dw)
+            b_new = self.__b - self.__b * db
+
+            if np.sum(w_new - self.__w) <= 0.001 * n_samples and b_new - self.__b < 0.001:
+                break
+
+            self.__w = w_new
+            self.__b = b_new
 
 
     def predict(self, X):
-        pass
+        return np.dot(X, self.__w) + self.__b
 
 
     def score(self, X, y):
-        pass
+        y_predicted = self.predict(X)
+        y_mean = 0
+        RSS = 0
+        TSS = 0
+
+        for l in y:
+            y_mean += l
+
+        y_mean = y_mean/len(y)
+
+        for i in range(len(y)):
+            RSS += pow(y[i] - y_predicted[i], 2)
+            TSS += pow(y[i] - y_mean, 2)
+
+        return (1 - (RSS/TSS))
